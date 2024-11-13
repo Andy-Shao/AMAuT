e9c1c62bfa6451c640c5df8881cf457a947d58bb
Andy-Shao <hj13035952619@126.com>
2024-11-13 17:14:36 +0000

    add AuT back

---
+++ AuT/script/pre_train.sh
@@ -0,0 +1,5 @@
+export BASE_PATH=${BASE_PATH:-'/root'}
+
+python -m AuT.pre_train --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' --max_epoch 70 \
+    --interval 1 --batch_size 64 --trte full --temporary_path $BASE_PATH'/tmp/AudioMNIST_train' \
+    --test_rate 0.3 --lr_gamma 10.0 --wandb
