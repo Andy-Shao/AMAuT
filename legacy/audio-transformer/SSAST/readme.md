cf72a081cf3ba17a1402eb82596b29aaaad0b8f9
Andy-Shao <hj13035952619@126.com>
2024-12-16 17:41:45 +0000

    update the models

--- legacy/audio-transformer/SSAST/readme.md
+++ legacy/audio-transformer/SSAST/readme.md
@@ -8,5 +8,14 @@ conda install -y -c anaconda scipy==1.11.3
 conda install conda-forge::timm==1.0.12 -y
 ```
 
+```shell
+conda create --name my_test2 python=3.9 -y
+conda activate my_test2
+# CUDA 11.3
+conda install pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cudatoolkit=11.3 -c pytorch -c conda-forge
+# conda install conda-forge::timm==0.4.5 -y
+pip install timm==0.4.5
+```
+
 # Cod Reference
 + [SSAST](https://github.com/YuanGongND/ssast/tree/main)
--- legacy/audio-transformer/SSAST/src/models/ast_models.py
+++ legacy/audio-transformer/SSAST/src/models/ast_models.py
@@ -3,7 +3,11 @@ BASE_PATH='/home/andyshao'
 sys.path.append(f"{BASE_PATH}/data/sls/scratch/aed-trans/src/models/")
 sys.path.append(f"{BASE_PATH}/data/sls/scratch/aed-trans/src/")
 from timm.models.layers import to_2tuple
+import numpy as np
+import timm
+from typing import Optional, Callable, Tuple, Union
 
+import torch
 import torch.nn as nn
 
 # override the timm package to relax the input shape constraint.
@@ -24,3 +28,100 @@ class PatchEmbed(nn.Module):
     def forward(self, x):
         x = self.proj(x).flatten(2).transpose(1, 2)
         return x
+    
+class PatchEmbed2(nn.Module):
+    """ 2D Image to Patch Embedding
+    """
+    def __init__(
+            self,
+            img_size: Optional[int] = 224,
+            patch_size: int = 16,
+            in_chans: int = 3,
+            embed_dim: int = 768,
+            norm_layer: Optional[Callable] = None,
+            flatten: bool = True,
+            output_fmt: Optional[str] = None,
+            bias: bool = True,
+            strict_img_size: bool = True,
+            dynamic_img_pad: bool = False,
+    ):
+        super().__init__()
+        self.patch_embed = PatchEmbed(
+            img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim
+        )
+
+    def forward(self,x):
+        return self.patch_embed(x)
+
+    def _init_img_size(self, img_size: Union[int, Tuple[int, int]]):
+        pass
+
+    def set_input_size(
+            self,
+            img_size: Optional[Union[int, Tuple[int, int]]] = None,
+            patch_size: Optional[Union[int, Tuple[int, int]]] = None,
+    ):
+        pass
+
+    def feat_ratio(self, as_scalar=True) -> Union[Tuple[int, int], int]:
+        pass
+
+    def dynamic_feat_size(self, img_size: Tuple[int, int]) -> Tuple[int, int]:
+        pass
+
+
+def get_sinusoid_encoding(n_position, d_hid):
+    ''' Sinusoid position encoding table '''
+
+    def get_position_angle_vec(position):
+        return [position / np.power(10000, 2 * (i // 2) / d_hid) for i in range(d_hid)]
+    
+    sinusoid_table = np.array([get_position_angle_vec(pos_i) for pos_i in range(n_position)])
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
+
+    return torch.FloatTensor(sinusoid_table).unsqueeze(0)
+
+class ASTModel(nn.Module):
+    def __init__(self, label_dim=527,
+                 fshape=128, tshape=2, fstride=128, tstride=2,
+                 input_fdim=128, input_tdim=1024, model_size='base',
+                 pretrain_stage=True, load_pretrained_mdl_path=None):
+        super(ASTModel, self).__init__()
+        # assert timm.__version__ == '0.4.5', 'Please use timm == 0.4.5, the code might not be compatible with newer versions.'
+
+        # override timm input shape restriction
+        timm.models.vision_transformer.PatchEmbed = PatchEmbed
+
+        # pretrain the AST models
+        if pretrain_stage == True:
+            if load_pretrained_mdl_path != None:
+                raise ValueError('Setting load_pretrained_mdl_path at pretraining stage is useless, pretraining is always from scratch, please change it to None.')
+            if fstride != fshape or tstride != tshape:
+                raise ValueError('fstride != fshape or tstride != tshape, they must be same at the pretraining stage, patch split overlapping is not supported.')
+            
+            # if AudioSet pretraining is not used (but ImageNet pretraining may still apply)
+            if model_size == 'tiny':
+                self.v = timm.create_model('vit_deit_tiny_distilled_patch16_224', pretrained=False)
+                self.heads, self.depth = 3, 12
+                self.cls_token_num = 2
+            elif model_size == 'small':
+                self.v = timm.create_model('vit_deit_small_distilled_patch16_224', pretrained=False)
+                self.heads, self.depth = 6, 12
+                self.cls_token_num = 2
+            elif model_size == 'base':
+                self.v = timm.create_model('vit_deit_base_distilled_patch16_384', pretrained=False)
+                self.heads, self.depth = 12, 12
+                self.cls_token_num = 2
+            elif model_size == 'base_nokd':
+                self.v = timm.create_model('vit_deit_base_patch16_384', pretrained=False)
+                self.heads, self.depth = 12, 12
+                self.cls_token_num = 1
+            else:
+                raise Exception('Model size must be one of tiny, small, base, base_nokd')
+            
+print('Testing')
+ASTModel(label_dim=527,
+         fshape=16, tshape=16, fstride=16, tstride=16,
+         input_fdim=128, input_tdim=1024, model_size='base',
+         pretrain_stage=True, load_pretrained_mdl_path=None)
