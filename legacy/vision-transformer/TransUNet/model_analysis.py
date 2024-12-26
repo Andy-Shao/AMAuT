8aebfc097cb6f4fd2ae93e82dcdad78a06cab520
Andy-Shao <hj13035952619@126.com>
2024-12-26 11:35:54 +0000

    add transunet print

--- legacy/vision-transformer/TransUNet/model_analysis.py
+++ legacy/vision-transformer/TransUNet/model_analysis.py
@@ -1,2 +1,20 @@
 import argparse
+import os
 
+from lib.models import TransUNet
+from lib.toolkits import print_model
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser('TransUnet')
+    ap.add_argument('--output_path', type=str, default='./output')
+
+    args = ap.parse_args()
+
+    try:
+        if not os.path.exists(args.output_path): os.makedirs(args.output_path)
+    except:
+        pass
+    ##############################################################
+
+    transUNet = TransUNet()
+    print_model(model=transUNet, output_path=os.path.join(args.output_path, 'TransUNet.txt'))
