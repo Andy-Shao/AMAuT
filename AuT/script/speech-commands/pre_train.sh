982be75af25f5c712c34061f3d6e56f84c0022f7
Andy-Shao <hj13035952619@126.com>
2025-01-24 13:22:27 +0000

    adjust code

--- AuT/script/speech-commands/pre_train.sh
+++ AuT/script/speech-commands/pre_train.sh
@@ -2,4 +2,4 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m AuT.speech-commands.pre_train --dataset 'speech-commands' --dataset_root_path $BASE_PATH'/data/speech_commands' \
     --output_csv_name 'training_records.csv' --output_weight_prefix 'speech-commands' --max_epoch 40 --interval_num 40 \
-    --batch_size 32 --lr '1e-3' --embed_mode 'CT' --embed_size 768 --wandb --early_stop 20
+    --batch_size 32 --lr '1e-3' --embed_mode 'CTA' --embed_size 768 --wandb
