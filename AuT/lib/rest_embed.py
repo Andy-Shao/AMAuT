c04fca979851362fee87c87a0c988280a3540292
Andy-Shao <hj13035952619@126.com>
2025-02-03 18:16:41 +0000

    fix bugs

--- AuT/lib/rest_embed.py
+++ AuT/lib/rest_embed.py
@@ -11,7 +11,7 @@ class Embedding(nn.Module):
         assert width % ng == 0, 'width must be dividable by the num_groups in GroupNorm.'
         self.restnet = RestNet(cin=num_channels, embed_size=embed_size, width=width, ng=ng, num_layers=num_layers)
         self.drop_out = nn.Dropout(p=marsked_rate)
-        self.patch_embedding = nn.Conv1d(in_channels=width*8, out_channels=embed_size, kernel_size=1, stride=1, padding=0)
+        self.patch_embedding = nn.Conv1d(in_channels=width*(2**(1+len(in_shape))), out_channels=embed_size, kernel_size=1, stride=1, padding=0)
         self.pos_embed = nn.Parameter(torch.zeros(1, in_shape[0], in_shape[1]))
         torch.nn.init.trunc_normal_(self.pos_embed, std=.02)
 
@@ -23,6 +23,35 @@ class Embedding(nn.Module):
         x = x.transpose(2, 1)
 
         return x, hidden_attens
+    
+    
+class RestNetCT(nn.Module):
+    def __init__(self, cin:int, width:int, ng:int, num_layers:list[int]):
+        super(RestNetCT, self).__init__()
+        self.root = nn.Sequential(
+            StdConv1d(in_channels=cin, out_channels=width, kernel_size=7, stride=2, bias=False, padding=3),
+            nn.GroupNorm(ng, width, eps=1e-6),
+            nn.ReLU()
+        )
+        self.maxPool = nn.MaxPool1d(kernel_size=3, stride=2, padding=0)
+
+        w = width
+        self.layers = nn.ModuleList()
+        for i, layer_num in enumerate(num_layers):
+            if i == 0:
+                self.layers.append(RestNetBlock(cin=w, cout=w*4, cmid=w, ng=ng))
+                for _ in range(layer_num): self.layers.append(RestNetBlock(cin=w*4, cout=w*4, cmid=w, ng=ng))
+                w = w * 4
+            else:
+                self.layers.append(RestNetBlock(cin=w, cout=w*2, cmid=w//2, stride=2, ng=ng))
+                for _ in range(layer_num): self.layers.append(RestNetBlock(cin=w*2, cout=w*2, cmid=w//2, ng=ng))
+                w = w * 2
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        x = self.root(x)
+        x = self.maxPool(x)
+        for l in self.layers: x = l(x)
+        return x
     
 class RestNet(nn.Module):
     def __init__(self, cin:int, embed_size:int, width:int, ng:int, num_layers:list[int]) -> None:
@@ -115,12 +144,12 @@ class RestNetBlock(nn.Module):
         y = self.relu(residual + y)
         return y
 
-def conv1x1(cin:int, cout:int, stride=1, bias=False) -> nn.Conv2d:
+def conv1x1(cin:int, cout:int, stride=1, bias=False) -> nn.Conv1d:
     return StdConv1d(
         in_channels=cin, out_channels=cout, kernel_size=1, stride=stride, padding=0, bias=bias
     )
 
-def conv3x3(cin:int, cout:int, stride=1, groups=1, bias=False) -> nn.Conv2d:
+def conv3x3(cin:int, cout:int, stride=1, groups=1, bias=False) -> nn.Conv1d:
     return StdConv1d(
         in_channels=cin, out_channels=cout, kernel_size=3, stride=stride, padding=1, bias=bias,
         groups=groups
