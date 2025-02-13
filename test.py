73ff1ffb6619bbc18cbae6d8c696672a6b7b404c
Andy-Shao <hj13035952619@126.com>
2025-02-13 10:40:45 +0000

    add test code

--- test.py
+++ test.py
@@ -1,28 +1,14 @@
-import soundfile as sf
+from pytube import YouTube
 
-from transformers import Wav2Vec2Processor, Wav2Vec2Model
-import torch 
-import torchaudio
+video_id = '---g-f_I2yQ'
+yt = YouTube("https://www.youtube.com/watch?v=" + video_id)
+stream = yt.streams.get_highest_resolution()
+file_path = stream.download(output_path='./')
+print(file_path)
 
-from lib.scDataset import SpeechCommandsDataset
+# pip install yt-dlp
+# conda install ffmpeg
 
-dataset = SpeechCommandsDataset(root_path='/home/andyshao/data/speech_commands', include_rate=True, data_type='all')
+# yt-dlp --format bestvideo+bestaudio --merge-output-format mp4 -o "videos/%(id)s.%(ext)s" https://www.youtube.com/watch?v=---g-f_I2yQ
 
-wavform, label, sample_rate = dataset[0]
-if sample_rate != 16000:
-    wavform = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)(wavform)
-wavform = wavform / torch.abs(wavform).max()
-
-# facebook/wav2vec2-base-960h
-processor = Wav2Vec2Processor.from_pretrained('facebook/wav2vec2-base')
-model = Wav2Vec2Model.from_pretrained('facebook/wav2vec2-base')
-
-# Preprocess and forward pass
-inputs = processor(wavform.squeeze().numpy(), sample_rate=16000, return_tensors='pt', padding=True)
-with torch.no_grad():
-    outputs = model(**inputs)
-
-# Extract embeddings
-embeddings = outputs.last_hidden_state # Shape: [batch_size, sequence_length, hidden_size]
-pooled_embeddings = torch.mean(embeddings, dim=1) # Shape: [batchsize, hidden_size]
-print(f'embeddings shape:{embeddings.shape}, pooled_embeddings shape:{pooled_embeddings.shape}')
+# yt-dlp --format bestvideo+bestaudio --merge-output-format mp4 -o "videos/%(id)s.%(ext)s" https://www.youtube.com/watch?v=---g-f_I2yQ
