2b1c7df92db9a99e1fd0fba3d46536e22a14760f
Andy-Shao <hj13035952619@126.com>
2025-02-21 12:22:52 +0000

    fix bugs

--- AuT/script/speech-commands/tta_analysis.sh
+++ AuT/script/speech-commands/tta_analysis.sh
@@ -3,6 +3,6 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 python -m AuT.speech_commands.tta_analysis --dataset 'speech-commands' --dataset_root_path $BASE_PATH'/data/speech_commands' \
     --corrupted_data_root_path $BASE_PATH'/tmp/speech-commands/doing_the_dishes-bg/3.0' \
     --output_csv_name 'tta_analysis.csv' --batch_size 32 --arch 'CT' --arch_level 'base' \
-    --original_auT_weight_path './result/speech-commands/AuT/pre_train/AuT.pt' \
-    --original_auC_weight_path './result/speech-commands/AuT/pre_train/AuT-Cls.pt' \
+    --original_auT_weight_path './result/speech-commands/AuT/pre_train/CT-SC-auT.pt' \
+    --original_auC_weight_path './result/speech-commands/AuT/pre_train/CT-SC-cls.pt' \
     --data_type 'raw' --corruption 'doing_the_dishes' --severity_level 3.0
