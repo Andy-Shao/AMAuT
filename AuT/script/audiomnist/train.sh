f6b0424405cbd259d82e9bd0eb5e8e205ffd1608
Andy-Shao <andyshao90@gmail.com>
2025-03-12 04:00:40 +0800

    clean code

--- AuT/script/audiomnist/train.sh
+++ AuT/script/audiomnist/train.sh
@@ -2,16 +2,16 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 
 # python -m AuT.audiomnist.train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
 #     --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'CT' --arch_level 'base' --validation_mode 'test' \
-#     --file_name_suffix 'debug' --fold 0 --wandb
+#     --file_name_suffix 'debug' --wandb
 
 python -m AuT.audiomnist.train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
     --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'CT' --arch_level 'base' --validation_mode 'validate' \
-    --file_name_suffix '0' --fold 0
+    --file_name_suffix '0'
 
 python -m AuT.audiomnist.train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
     --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'CT' --arch_level 'base' --validation_mode 'validate' \
-    --file_name_suffix '1' --fold 0
+    --file_name_suffix '1'
 
 python -m AuT.audiomnist.train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
     --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'CT' --arch_level 'base' --validation_mode 'validate' \
-    --file_name_suffix '2' --fold 0
+    --file_name_suffix '2'
