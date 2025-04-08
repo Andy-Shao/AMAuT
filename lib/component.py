c71d03ad1afab42271c4d83ab3fd6e02b18f2357
Andy-Shao <andyshao90@gmail.com>
2025-04-08 22:18:54 +0800

    adjust the RMSNorm

--- lib/component.py
+++ lib/component.py
@@ -8,15 +8,19 @@ class RMSNorm(nn.Module):
      
     Original research: Root Mean Square Layer Normalization (https://proceedings.neurips.cc/paper/2019/hash/1e8a19426224ca89e83cef47f1e7f53b-Abstract.html)
     """
-    def __init__(self, dim:int, eps:float=1e-6):
+    def __init__(self, normalized_shape:list[int], eps:float=1e-6, elementwise_affine:bool=True):
         super(RMSNorm, self).__init__()
         self.eps = eps 
-        self.weight = nn.Parameter(torch.ones(dim))
+        
+        self.elementwise_affine = elementwise_affine
+        if elementwise_affine:
+            self.weight = nn.Parameter(torch.ones(normalized_shape))
 
     def __norm__(self, x:torch.Tensor) -> torch.Tensor:
         return x * torch.rsqrt(x.pow(2).mean(dim=-1, keepdim=True) + self.eps)
     
     def forward(self, x:torch.Tensor) -> torch.Tensor:
         x = self.__norm__(x)
-        x = x * self.weight
+        if self.elementwise_affine:
+            x = x * self.weight
         return x
