b881dd56aa0f60407c2b23be0af4c04765f13d43
Andy-Shao <hj13035952619@126.com>
2025-05-07 21:02:56 +0000

    finish AM analysis

--- AuT/script/audiomnist/cross_validate.sh
+++ AuT/script/audiomnist/cross_validate.sh
@@ -95,49 +95,49 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 ############
 # FCE
 ## fold 1
-# python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-#     --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
-#     --fold 1 --file_name_suffix '0' --wandb
+python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
+    --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
+    --fold 1 --file_name_suffix '0' --wandb
 
-# python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-#     --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
-#     --fold 1 --file_name_suffix '1'
+python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
+    --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
+    --fold 1 --file_name_suffix '1'
 
-# python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-#     --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
-#     --fold 1 --file_name_suffix '2'
+python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
+    --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
+    --fold 1 --file_name_suffix '2'
 
-# python -m AuT.audiomnist.fce_analysis --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-#     --batch_size 32 --arch 'FCE' --arch_level 'base' --fold 1 \
-#     --original_auT_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-auT0.pt' \
-#     --original_auC_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-cls0.pt' \
-#     --original_auT2_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-auT1.pt' \
-#     --original_auC2_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-cls1.pt' \
-#     --original_auT3_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-auT2.pt' \
-#     --original_auC3_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-cls2.pt' \
-#     --output_csv_name 'fce_analysis_f1.csv'
-# ## fold 2
-# python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-#     --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
-#     --fold 2 --file_name_suffix '0' --wandb
+python -m AuT.audiomnist.fce_analysis --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
+    --batch_size 32 --arch 'FCE' --arch_level 'base' --fold 1 \
+    --original_auT_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-auT0.pt' \
+    --original_auC_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-cls0.pt' \
+    --original_auT2_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-auT1.pt' \
+    --original_auC2_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-cls1.pt' \
+    --original_auT3_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-auT2.pt' \
+    --original_auC3_weight_path './result/AudioMNIST/AuT/train/1/FCE-AM-cls2.pt' \
+    --output_csv_name 'fce_analysis_f1.csv'
+## fold 2
+python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
+    --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
+    --fold 2 --file_name_suffix '0' --wandb
 
-# python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-#     --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
-#     --fold 2 --file_name_suffix '1'
+python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
+    --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
+    --fold 2 --file_name_suffix '1'
 
-# python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-#     --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
-#     --fold 2 --file_name_suffix '2'
+python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
+    --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
+    --fold 2 --file_name_suffix '2'
 
-# python -m AuT.audiomnist.fce_analysis --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
-#     --batch_size 32 --arch 'FCE' --arch_level 'base' --fold 2 \
-#     --original_auT_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-auT0.pt' \
-#     --original_auC_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-cls0.pt' \
-#     --original_auT2_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-auT1.pt' \
-#     --original_auC2_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-cls1.pt' \
-#     --original_auT3_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-auT2.pt' \
-#     --original_auC3_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-cls2.pt' \
-#     --output_csv_name 'fce_analysis_f2.csv'
+python -m AuT.audiomnist.fce_analysis --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
+    --batch_size 32 --arch 'FCE' --arch_level 'base' --fold 2 \
+    --original_auT_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-auT0.pt' \
+    --original_auC_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-cls0.pt' \
+    --original_auT2_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-auT1.pt' \
+    --original_auC2_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-cls1.pt' \
+    --original_auT3_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-auT2.pt' \
+    --original_auC3_weight_path './result/AudioMNIST/AuT/train/2/FCE-AM-cls2.pt' \
+    --output_csv_name 'fce_analysis_f2.csv'
 ## fold 3
 python -m AuT.audiomnist.fce_train --dataset 'AudioMNIST' --dataset_root_path $BASE_PATH'/data/AudioMNIST/data' \
     --max_epoch 30 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' --validation_mode 'test' \
