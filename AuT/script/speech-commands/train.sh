c9bcb9a6baf77c718b8729ad362fd475c3ee2940
Andy-Shao <hj13035952619@126.com>
2025-05-07 01:51:31 +0000

    finishing optimization

--- AuT/script/speech-commands/train.sh
+++ AuT/script/speech-commands/train.sh
@@ -22,19 +22,29 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --max_epoch 80 --lr_cardinality 50 --batch_size 32 --lr '1e-3' --arch 'CT' --arch_level 'base' \
 #     --file_name_suffix '3'
 
-# python -m AuT.speech_commands.fce_train --dataset 'speech-commands' --dataset_root_path $BASE_PATH'/data/speech_commands' \
-#     --max_epoch 50 --lr_cardinality 50 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' \
-#     --background_path $BASE_PATH'/data/speech_commands' --num_workers 16 --wandb
+python -m AuT.speech_commands.fce_train --dataset 'speech-commands' --dataset_root_path $BASE_PATH'/data/speech_commands' \
+    --max_epoch 50 --lr_cardinality 50 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' \
+    --background_path $BASE_PATH'/data/speech_commands' --num_workers 16 --wandb
 
-# python -m AuT.speech_commands.fce_train --dataset 'speech-commands' --dataset_root_path $BASE_PATH'/data/speech_commands' \
-#     --max_epoch 50 --lr_cardinality 50 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' \
-#     --background_path $BASE_PATH'/data/speech_commands' --num_workers 16 --file_name_suffix '2'
+python -m AuT.speech_commands.fce_train --dataset 'speech-commands' --dataset_root_path $BASE_PATH'/data/speech_commands' \
+    --max_epoch 50 --lr_cardinality 50 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' \
+    --background_path $BASE_PATH'/data/speech_commands' --num_workers 16 --file_name_suffix '2'
 
-# python -m AuT.speech_commands.fce_train --dataset 'speech-commands' --dataset_root_path $BASE_PATH'/data/speech_commands' \
-#     --max_epoch 50 --lr_cardinality 50 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' \
-#     --background_path $BASE_PATH'/data/speech_commands' --num_workers 16 --file_name_suffix '3'
+python -m AuT.speech_commands.fce_train --dataset 'speech-commands' --dataset_root_path $BASE_PATH'/data/speech_commands' \
+    --max_epoch 50 --lr_cardinality 50 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' \
+    --background_path $BASE_PATH'/data/speech_commands' --num_workers 16 --file_name_suffix '3'
 
-python -m AuT.speech_commands.fce_train --dataset 'speech-commands_v2' --dataset_root_path $BASE_PATH'/data' \
-    --max_epoch 30 --lr_cardinality 50 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' \
-    --background_path $BASE_PATH'/data/speech-commands_v2/speech_commands_v0.02' --num_workers 16 \
-    --wandb
+# python -m AuT.speech_commands.fce_train --dataset 'speech-commands_v2' --dataset_root_path $BASE_PATH'/data' \
+#     --max_epoch 50 --lr_cardinality 50 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' \
+#     --background_path $BASE_PATH'/data/speech-commands_v2/speech_commands_v0.02' --num_workers 16 \
+#     --wandb
+
+# python -m AuT.speech_commands.fce_train --dataset 'speech-commands_v2' --dataset_root_path $BASE_PATH'/data' \
+#     --max_epoch 50 --lr_cardinality 50 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' \
+#     --background_path $BASE_PATH'/data/speech-commands_v2/speech_commands_v0.02' --num_workers 16 \
+#     --file_name_suffix '2'
+
+# python -m AuT.speech_commands.fce_train --dataset 'speech-commands_v2' --dataset_root_path $BASE_PATH'/data' \
+#     --max_epoch 50 --lr_cardinality 50 --batch_size 32 --lr '1e-3' --arch 'FCE' --arch_level 'base' \
+#     --background_path $BASE_PATH'/data/speech-commands_v2/speech_commands_v0.02' --num_workers 16 \
+#     --file_name_suffix '3'
--- AuT/script/speech-commands/tta_analysis.sh
+++ AuT/script/speech-commands/tta_analysis.sh
@@ -18,16 +18,20 @@ export BASE_PATH=${BASE_PATH:-'/root'}
 #     --original_auT3_weight_path './result/speech-commands_v2/AuT/train/CT-SC2-auT3.pt' \
 #     --original_auC3_weight_path './result/speech-commands_v2/AuT/train/CT-SC2-cls3.pt'
 
-# python -m AuT.speech_commands.fce_analysis --dataset 'speech-commands' --dataset_root_path $BASE_PATH'/data/speech_commands' \
-#     --output_csv_name 'fce_analysis.csv' --batch_size 32 --arch 'FCE' --arch_level 'base' \
-#     --original_auT_weight_path './result/speech-commands/AuT/train/FCE-SC-auT.pt' \
-#     --original_auC_weight_path './result/speech-commands/AuT/train/FCE-SC-cls.pt' \
-#     --original_auT2_weight_path './result/speech-commands/AuT/train/FCE-SC-auT2.pt' \
-#     --original_auC2_weight_path './result/speech-commands/AuT/train/FCE-SC-cls2.pt' \
-#     --original_auT3_weight_path './result/speech-commands/AuT/train/FCE-SC-auT3.pt' \
-#     --original_auC3_weight_path './result/speech-commands/AuT/train/FCE-SC-cls3.pt'
-
-python -m AuT.speech_commands.fce_analysis --dataset 'speech-commands_v2' --dataset_root_path $BASE_PATH'/data' \
+python -m AuT.speech_commands.fce_analysis --dataset 'speech-commands' --dataset_root_path $BASE_PATH'/data/speech_commands' \
     --output_csv_name 'fce_analysis.csv' --batch_size 32 --arch 'FCE' --arch_level 'base' \
-    --original_auT_weight_path './result/speech-commands_v2/AuT/train/FCE-SC2-auT.pt' \
-    --original_auC_weight_path './result/speech-commands_v2/AuT/train/FCE-SC2-cls.pt' --only_origin
+    --original_auT_weight_path './result/speech-commands/AuT/train/FCE-SC-auT.pt' \
+    --original_auC_weight_path './result/speech-commands/AuT/train/FCE-SC-cls.pt' \
+    --original_auT2_weight_path './result/speech-commands/AuT/train/FCE-SC-auT2.pt' \
+    --original_auC2_weight_path './result/speech-commands/AuT/train/FCE-SC-cls2.pt' \
+    --original_auT3_weight_path './result/speech-commands/AuT/train/FCE-SC-auT3.pt' \
+    --original_auC3_weight_path './result/speech-commands/AuT/train/FCE-SC-cls3.pt'
+
+# python -m AuT.speech_commands.fce_analysis --dataset 'speech-commands_v2' --dataset_root_path $BASE_PATH'/data' \
+#     --output_csv_name 'fce_analysis.csv' --batch_size 32 --arch 'FCE' --arch_level 'base' \
+#     --original_auT_weight_path './result/speech-commands_v2/AuT/train/FCE-SC2-auT.pt' \
+#     --original_auC_weight_path './result/speech-commands_v2/AuT/train/FCE-SC2-cls.pt' \
+#     --original_auT2_weight_path './result/speech-commands_v2/AuT/train/FCE-SC2-auT2.pt' \
+#     --original_auC2_weight_path './result/speech-commands_v2/AuT/train/FCE-SC2-cls2.pt' \
+#     --original_auT3_weight_path './result/speech-commands_v2/AuT/train/FCE-SC2-auT3.pt' \
+#     --original_auC3_weight_path './result/speech-commands_v2/AuT/train/FCE-SC2-cls3.pt'
