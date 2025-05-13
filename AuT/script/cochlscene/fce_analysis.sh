09d3ed8507c0518c7cd5a150fe8b562d9433356c
Andy-Shao <hj13035952619@126.com>
2025-05-13 14:28:40 +0000

    optimize cochlscene

--- AuT/script/cochlscene/fce_analysis.sh
+++ AuT/script/cochlscene/fce_analysis.sh
@@ -1,7 +1,7 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m AuT.cochlscene.fce_analysis --dataset 'CochlScene' --dataset_root_path $BASE_PATH'/data/CochlScene' \
-    --output_csv_name 'fce_analysis' --batch_size 32 --arch 'FCE' --arch_level 'base' \
+    --output_csv_name 'fce_analysis.csv' --batch_size 32 --arch 'FCE' --arch_level 'base' \
     --original_auT_weight_path './result/CochlScene/AuT/train/FCE-CS-auT.pt' \
     --original_auC_weight_path './result/CochlScene/AuT/train/FCE-CS-cls.pt' \
     --original_auT2_weight_path './result/CochlScene/AuT/train/FCE-CS-auT2.pt' \
