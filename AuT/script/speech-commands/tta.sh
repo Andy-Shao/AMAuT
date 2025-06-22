c9e136a0049ab39075aecfb6d0a00f236fad0690
Andy-Shao <hj13035952619@126.com>
2025-06-22 14:04:01 +0000

    optimization

--- AuT/script/speech-commands/tta.sh
+++ AuT/script/speech-commands/tta.sh
@@ -2,7 +2,7 @@
 export BASE_PATH=${BASE_PATH:-'/root'}
 
 python -m AuT.speech_commands.tta --dataset 'speech-commands' --dataset_root_path $BASE_PATH'/data/speech_commands' \
-    --max_epoch 50 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --num_workers 16 --auT_lr_decay 0.55 --nucnm_rate 1.0 \
+    --max_epoch 50 --lr_cardinality 50 --batch_size 70 --lr '1e-4' --num_workers 16 --auT_lr_decay 0.45 --nucnm_rate 1.0 \
     --lr_gamma 30 --lr_threshold 35 --ent_rate 0.5 --gent_rate 0.5 --gent_q 1.1 --arch 'FCE' --arch_level 'base' \
     --origin_auT_weight './result/speech-commands/AuT/train/FCE-SC-auT.pt' \
     --origin_cls_weight './result/speech-commands/AuT/train/FCE-SC-cls.pt' --wandb
