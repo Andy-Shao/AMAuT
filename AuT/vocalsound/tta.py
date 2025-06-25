fabd0d507db9fa10ac799bf5f02f8b896cd136df
Andy-Shao <hj13035952619@126.com>
2025-06-25 11:01:54 +0000

    optimization

--- AuT/vocalsound/tta.py
+++ AuT/vocalsound/tta.py
@@ -188,8 +188,8 @@ if __name__ == '__main__':
         print(f'Accuracy is: {accuracy:.4f}%, sample size is: {len(shift_set)}')
         if accuracy >= max_accu:
             max_accu = accuracy
-            torch.save(auTmodel.state_dict(), os.path.join(args.output_path, f'{arch}-{dataset_tag(args.dataset)}-auT{args.file_suffix}.pt'))
-            torch.save(clsmodel.state_dict(), os.path.join(args.output_path, f'{arch}-{dataset_tag(args.dataset)}-cls{args.file_suffix}.pt'))
+        torch.save(auTmodel.state_dict(), os.path.join(args.output_path, f'{arch}-{dataset_tag(args.dataset)}-auT{args.file_suffix}.pt'))
+        torch.save(clsmodel.state_dict(), os.path.join(args.output_path, f'{arch}-{dataset_tag(args.dataset)}-cls{args.file_suffix}.pt'))
 
         wandb_run.log(
             data={
