f4582d33f6308dca92b8a3cd6da7a1aa8418ecc8
Andy-Shao <hj13035952619@126.com>
2025-10-23 12:23:21 +0000

    add consistency analysis for VS

---
+++ AuT/script/vocalsound/cons_analy.sh
@@ -0,0 +1,10 @@
+export BASE_PATH=${BASE_PATH:-'/root'}
+
+python -m AuT.vocalsound.cons_analy --dataset 'VocalSound' --dataset_root_path $BASE_PATH'/data/vocalsound_16k' \
+    --batch_size 32 --arch 'FCE' --arch_level 'base' --output_csv_name 'fce_analysis.csv' \
+    --original_auT_weight_path './result/VocalSound/AuT/train/FCE-VS-auT.pt' \
+    --original_auC_weight_path './result/VocalSound/AuT/train/FCE-VS-cls.pt' \
+    --original_auT2_weight_path './result/VocalSound/AuT/train/FCE-VS-auT2.pt' \
+    --original_auC2_weight_path './result/VocalSound/AuT/train/FCE-VS-cls2.pt' \
+    --original_auT3_weight_path './result/VocalSound/AuT/train/FCE-VS-auT3.pt' \
+    --original_auC3_weight_path './result/VocalSound/AuT/train/FCE-VS-cls3.pt'
