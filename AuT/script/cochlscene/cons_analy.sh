5a2cbe148f951f7639a0db1a2d5559453a8d320c
Andy-Shao <hj13035952619@126.com>
2025-10-24 07:09:18 +0000

    add consistency analysis for CS

---
+++ AuT/script/cochlscene/cons_analy.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+export BASE_PATH=${BASE_PATH:-'/root'}
+
+python -m AuT.cochlscene.cons_analy --dataset 'CochlScene' --dataset_root_path $BASE_PATH'/data/CochlScene' \
+    --output_csv_name 'fce_analysis.csv' --batch_size 32 --arch 'FCE' --arch_level 'base' \
+    --original_auT_weight_path './result/CochlScene/AuT/train/FCE-CS-auT.pt' \
+    --original_auC_weight_path './result/CochlScene/AuT/train/FCE-CS-cls.pt' \
+    --original_auT2_weight_path './result/CochlScene/AuT/train/FCE-CS-auT2.pt' \
+    --original_auC2_weight_path './result/CochlScene/AuT/train/FCE-CS-cls2.pt' \
+    --original_auT3_weight_path './result/CochlScene/AuT/train/FCE-CS-auT3.pt' \
+    --original_auC3_weight_path './result/CochlScene/AuT/train/FCE-CS-cls3.pt'
